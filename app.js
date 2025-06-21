a0d1d0f51a8466613010004d61471d28a1e43414
harsh_sde <harsh_s.dev@outlook.com>
2025-06-21 19:15:35 +0530

    component composition

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = <h1 className="head">Namaste I am JSX 🚀</h1>;
+const heading = <h1 className="head">Namaste I am React Element 🚀</h1>;
 
 const Title = () => (
   <h1 className="title">Learning React from Akshay Saini💥</h1>
@@ -9,16 +9,16 @@ const Title = () => (
 
 const HeadComponent = function () {
   return (
-    <div>
+    <div id="container">
       <Title />
-      <h1 className="headComponent">I am functional component 🚀</h1>;
+      {heading}
+      <h1 className="headComponent">I am functional component 🚀</h1>
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    {heading}
     <HeadComponent />
   </>
 );
