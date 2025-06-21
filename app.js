a014456cb777bcdb1b399a68681159f22f09833c
harsh_sde <harsh_s.dev@outlook.com>
2025-06-21 18:58:19 +0530

    Update component

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ import ReactDOM from "react-dom/client";
 
 const heading = <h1 className="head">Namaste I am JSX 🚀</h1>;
 
+const Title = () => (
+  <h1 className="title">Learning React from Akshay Saini💥</h1>
+);
+
 const HeadComponent = function () {
-  return <h1 className="headComponent">I am functional component 🚀</h1>;
+  return (
+    <div>
+      <Title />
+      <h1 className="headComponent">I am functional component 🚀</h1>;
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root")).render(
