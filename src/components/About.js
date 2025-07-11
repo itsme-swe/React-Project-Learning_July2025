1d8d3443e45029c89c5a6a30b7b42ab424df79d2
itsharsh_swe <harsh_s.dev@outlook.com>
2025-07-11 11:53:22 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,36 @@
 import User from "./UserFunctionalComponent";
 import UserClassComponent from "./UserClassComponent";
+import React from "react";
 
-export const About = () => {
-  return (
-    <div className="about-container">
-      <h1>About our F🍛🍛d Standards </h1>
-      {User()}
+class About extends React.Component {
+  render() {
+    return (
+      <div className="about-container">
+        <h1>About our F🍛🍛d Standards </h1>
+        <UserClassComponent
+          name={"Vasu Mehra"}
+          location={"Ajmer, Raj"}
+          contact={"vasu@gmail.com"}
+        />
+        <User />
+      </div>
+    );
+  }
+}
 
-      <UserClassComponent
-        name={"Vasu Mehra"}
-        location={"Ajmer,Raj."}
-        contact={"vasu@gmail.com"}
-      />
-    </div>
-  );
-};
+export default About;
+
+// export const About = () => {
+//   return (
+//     <div className="about-container">
+//       <h1>About our F🍛🍛d Standards </h1>
+//       {User()}
+
+//       <UserClassComponent
+//         name={"Vasu Mehra"}
+//         location={"Ajmer,Raj."}
+//         contact={"vasu@gmail.com"}
+//       />
+//     </div>
+//   );
+// };
