5d76e767feac3245dea1ff12e5edeaf522e100dd
itsharsh_swe <harsh_s.dev@outlook.com>
2025-07-12 11:24:01 +0530

    update

--- src/utils/useOnlineStatus.js
+++ src/utils/useOnlineStatus.js
@@ -20,3 +20,19 @@ const useOnlineStatus = () => {
 };
 
 export default useOnlineStatus;
+
+/*
+🔸 The useOnlineStatus hook helps us to detect if the user is connected to the internet or not.
+
+  It returns:
+  
+  true → when the device is online
+  
+  false → when the device is offline
+
+  It also cleans up the event listeners when the component using the hook is removed, to avoid memory leaks.
+
+In this app this hook is used inside the Header Component as nav-bar element.
+
+
+*/
