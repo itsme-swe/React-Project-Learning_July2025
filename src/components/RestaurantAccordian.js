a4c3d69f0ce4bfc03be1c4c19f2e070a7f833329
itsharsh_swe <harsh_s.dev@outlook.com>
2025-07-16 22:05:21 +0530

    update

---
+++ src/components/RestaurantAccordian.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const RestaurantAccordian = ({ data }) => {
+  console.log(data);
+  return <div>Restaurant Category</div>;
+};
+
+export default RestaurantAccordian;
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,6 +1,7 @@
 import { Shimmer } from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantAccordian from "./RestaurantAccordian";
 
 const RestaurantMenu = () => {
   const { restId } = useParams();
@@ -23,21 +24,27 @@ const RestaurantMenu = () => {
 
   const itemCards = menuCard?.itemCards || [];
 
+  const categories =
+    regularCards?.filter((c) => {
+      const type = c.card?.card?.["@type"];
+      return (
+        type ===
+          "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory" ||
+        type ===
+          "type.googleapis.com/swiggy.presentation.food.v2.NestedItemCategory"
+      );
+    }) || [];
+
   return (
     <div className="menu">
       <h1>{name}</h1>
       <h3>{cuisines.join(", ")}</h3>
       <h3>{costForTwoMessage}</h3>
       <h2>Menu</h2>
-      <ul>
-        {itemCards.map((item) => (
-          <li key={item.card.info.id}>
-            {item.card.info.name} -
-            {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
-            {}
-          </li>
-        ))}
-      </ul>
+
+      {categories.map((ctg) => (
+        <RestaurantAccordian data={ctg.card?.card} />
+      ))}
     </div>
   );
 };
