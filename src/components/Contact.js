5b90092ed6aa7de3b8935e39ce420bd63622f1cd
itsharsh_swe <harsh_s.dev@outlook.com>
2025-08-08 11:54:50 +0530

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,23 @@
 export const Contact = () => {
   return (
     <div>
-      <h1>Contact Us Page</h1>
+      <h1 className="font-bold text-2xl p-4 m-4">Contact Us Page</h1>
+
+      <form>
+        <input
+          type="text"
+          className="border border-black p-2 m-2"
+          placeholder="Name"
+        />
+        <input
+          type="text"
+          className="border border-black p-2 m-2"
+          placeholder="Message"
+        />
+        <button className="border border-black p-2 m-2 bg-amber-100 rounded-lg">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
---
+++ src/components/__tests__/contact.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import { Contact } from "../Contact";
+import "@testing-library/jest-dom";
+
+test("Should load Contact Us component", () => {
+  render(<Contact />);
+
+  const heading = screen.getByRole("heading");
+
+  // Assertion
+  expect(heading).toBeInTheDocument();
+});
+
+test("Should load button inside Contact Component", () => {
+  render(<Contact />);
+
+  const btn = screen.getByText("Submit"); // even we used getByText() also
+
+  expect(btn).toBeInTheDocument();
+});
+
+test("Should load input name inside Contact component", () => {
+  render(<Contact />);
+
+  const inputName = screen.getByPlaceholderText("Name");
+
+  expect(inputName).toBeInTheDocument();
+});
